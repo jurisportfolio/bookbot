ac3a9801cf7fe3283961e81fa0952b2a2798a7c3
jurko <jurko.bossetroll@gmail.com>
2024-02-29 11:28:01 +0100

    Add counting words and letters in book

--- main.py
+++ main.py
@@ -9,4 +9,19 @@ def get_book_text(path):
         return file.read()
 
 
+def get_num_words(text: str):
+    words = text.split()
+    return len(words)
+
+
+def get_num_letters(text: str):
+    lowered_string = text.lower()
+    letter_appearance = {}
+    for letter in lowered_string:
+        if not letter in letter_appearance:
+            letter_appearance[letter] = 1
+        letter_appearance[letter] += 1
+    return letter_appearance
+
+
 main()
